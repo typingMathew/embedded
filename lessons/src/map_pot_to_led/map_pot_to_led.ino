d1b268520dda992571e35974e8fc166af9fc772a
typingMathew <matteo.o.1106@gmail.com>
2024-04-26 09:39:28 +0200

    simplified programm and renamed mds

--- lessons/src/map_pot_to_led/map_pot_to_led.ino
+++ lessons/src/map_pot_to_led/map_pot_to_led.ino
@@ -1,10 +1,11 @@
 void setup(){
-    // Digital PINs 0 to 7 are used for the LEDs
-    for (int i = 0; i < 8; i++) {
-        pinMode(i, OUTPUT);
-    }
+    // set all pins of PORTD as output
+    DDRD = 0xFF;
     // Analog PIN A0 is used for the potentiometer
     pinMode(A0, INPUT);
+
+    // Dies ist auch direkt über das DDRC Register möglich:
+    // DDRC = 0x00;
 }
 
 void loop(){
@@ -13,7 +14,8 @@ void loop(){
     // map the 10 Bit value to 8 Bit value
     int ledValue = map(value, 0, 1023, 0, 255);
     // display the 8 Bit value on the LEDs
-    for (int i = 0; i < 8; i++) {
-        digitalWrite(i, bitRead(ledValue, i));
-    }
+    PORTD = ledValue;
+
+    // Diese drei Befehle lassen sich auch zusammenfassen:
+    // PORTD = map(analogRead(A0), 0, 1023, 0, 255);
 }
